b78064e03bcb20d416055e0c284cd6a3474d2235
alittleton98 <andrew.littleton1998@gmail.com>
2020-12-07 17:17:22 -0600

    remaining models created

---
+++ eLoan/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace eLoan_Project.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int customer_ID { get; set; }
+        public int application_ID { get; set; }
+        public int profile_ID { get; set; }
+        public int loan_ID { get; set; }
+
+        public Customer()
+        {
+        }
+    }
+}
---
+++ eLoan/Models/Login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace eLoan_Project.Models
+{
+    public class Login
+    {
+        [Key]
+        public string email_address { get; set; }
+        public string password { get; set; }
+        public string customer_ID { get; set; }
+        public Login()
+        {
+        }
+    }
+}
